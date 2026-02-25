89ffee4439954a6dbbdbdd0999a11c14d476f7b9
trcrsired <uwgghhbcad@gmail.com>
2026-02-25 12:34:41 +0800

    Add more iptvchannels

--- iptvchannels.js
+++ iptvchannels.js
@@ -6,12 +6,109 @@ export const iptvChannels = [
       "https://d1ewctnvcwvvvu.cloudfront.net/playlist.m3u8"
     ]
   },
-  { name: "PBS Nature",
-    urls: [ "https://amg01597-wnetnewyorkpubl-pbsnature-xumo-8xorq.amagi.tv/playlist.m3u8" ]
-  },
   { name: "RT",
     urls: [ "https://rumble.com/live-hls-dvr/33aw1a/playlist.m3u8" ]
   },
+  {
+    name: "PressTV",
+    urls: [
+      "https://lenz.splus.ir/PLTV/88888888/224/3221226156/index.m3u8"
+    ]
+  },
+  {
+    name: "Baywatch (1080p)",
+    urls: [
+      "https://amg00145-fremantlemedian-baywatch-samsungnz-5bfqe.amagi.tv/playlist/amg00145-fremantlemedian-baywatch-samsungnz/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Breaking News by LeadStory (1080p)",
+    urls: [
+      "https://amg02703-leadstory-leadstory-samsungnz-vc7nn.amagi.tv/playlist/amg02703-leadstory-leadstory-samsungnz/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Deal or No Deal (1080p)",
+    urls: [
+      "https://amg00627-amg00627c21-samsung-nz-3876.playouts.now.amagi.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Four in a Bed (720p)",
+    urls: [
+      "https://all3media-four-in-a-bed-1-nz.samsung.wurl.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "France 24 (1080p)",
+    urls: [
+      "https://amg00106-amg00106c1-samsung-nz-4151.playouts.now.amagi.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "PBS National East (1080p)",
+    urls: [
+      "https://pbs.lls.cdn.pbs.org/est/index.m3u8"
+    ]
+  },
+  {
+    name: "PBS History (1080p)",
+    urls: [
+      "https://amg02333-pbs-amg02333c1-samsung-nz-1254.playouts.now.amagi.tv/playlist/amg02333-pbsdistributionfast-pbshistory-samsungnz/playlist.m3u8"
+    ]
+  },
+  {
+    name: "PBS Antiques Road Trip (1080p)",
+    urls: [
+      "https://amg02333-pbs-amg02333c5-samsung-nz-2440.playouts.now.amagi.tv/PBSD-AntiquesRoadTripSF-samsung/playlist.m3u8"
+    ]
+  },
+  { name: "PBS Nature",
+    urls: [ "https://amg01597-wnetnewyorkpubl-pbsnature-xumo-8xorq.amagi.tv/playlist.m3u8" ]
+  },
+  {
+    name: "Outdoor Channel",
+    urls: [
+      "https://cdn-apse1-prod.tsv2.amagi.tv/linear/amg00718-outdoorchannela-outdoortvnz-samsungnz/playlist.m3u8"
+    ]
+  },
+  {
+    name: "NOW Rock",
+    urls: [
+      "https://lightning-now90s-samsungnz.amagi.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Now 70's",
+    urls: [
+      "https://lightning-now70s-samsungnz.amagi.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Now 80's",
+    urls: [
+      "https://lightning-now80s-samsungnz.amagi.tv/playlist.m3u8"
+    ]
+  },
+  {
+    name: "NBC News Now",
+    urls: [
+      "https://xumo-drct-nbcnn-ir8ze.fast.nbcuni.com/live/master.m3u8"
+    ]
+  },
+  {
+    name: "NBC Sports",
+    urls: [
+      "https://xumo-xumoent-vc-122-sjv70.fast.nbcuni.com/live/master.m3u8"
+    ]
+  },
+  {
+    name: "NBC Comedy Vault",
+    urls: [
+      "https://xumo-xumoent-vc-105-z0vpm.fast.nbcuni.com/live/master.m3u8"
+    ]
+  },
+
   {
     name: "CCTV-4 America",
     urls: [
@@ -36,4 +133,290 @@ export const iptvChannels = [
       "https://news.cgtn.com/resource/live/english/cgtn-news.m3u8"
     ]
   },
+  {
+    name: "CCTV-16 (Audio Only) (CCTV16 只有声音)",
+    nameLocalized: { zhcn: "CCTV16 只有声音" },
+    urls: [
+      "https://piccpndali.v.myalicdn.com/audio/cctv16_2.m3u8"
+    ]
+  },
+  {
+    name: "CGTN Russian (CGTN俄语)",
+    nameLocalized: { zhcn: "CGTN俄语" },
+    urls: [
+      "https://0472.org/hls/cgtne.m3u8"
+    ]
+  },
+  {
+    name: "CGTN French (CGTN法语)",
+    nameLocalized: { zhcn: "CGTN法语" },
+    urls: [
+      "https://0472.org/hls/cgtnf.m3u8"
+    ]
+  },
+  {
+    name: "CGTN Documentary (CGTN纪录)",
+    nameLocalized: { zhcn: "CGTN纪录" },
+    urls: [
+      "https://0472.org/hls/cgtnd.m3u8",
+      "https://english-livetx.cgtn.com/hls/yypdjlctzb_hd.m3u8"
+    ]
+  },
+  {
+    name: "iPanda (熊猫频道)",
+    nameLocalized: { zhcn: "熊猫频道" },
+    urls: [
+      "https://gcwbndali.v.myalicdn.com/gcwbnd/ipanda1000_2/index.m3u8"
+    ]
+  },
+  {
+    name: "Live China (直播中国)",
+    nameLocalized: { zhcn: "直播中国" },
+    urls: [
+      "https://gcalic.v.myalicdn.com/gc/wgw05_1/index.m3u8?contentid=2820180516001"
+    ]
+  },
+  {
+    name: "Hunan TV (湖南卫视)",
+    nameLocalized: { zhcn: "湖南卫视" },
+    urls: [
+      "https://txmov2.a.kwimgs.com/bs3/video-hls/5199687338554291078_hlsb.m3u8"
+    ]
+  },
+  {
+    name: "Sansha Satellite TV (三沙卫视)",
+    nameLocalized: { zhcn: "三沙卫视" },
+    urls: [
+      "https://srs.ssws.tv/video/sstv-10/index.m3u8"
+    ]
+  },
+  {
+    name: "Dragon TV (东方卫视)",
+    nameLocalized: { zhcn: "东方卫视" },
+    urls: [
+      "https://txmov2.a.kwimgs.com/bs3/video-hls/5219953535631090825_hlsb.m3u8"
+    ]
+  },
+  {
+    name: "Shandong Education TV (山东教育)",
+    nameLocalized: { zhcn: "山东教育" },
+    urls: [
+      "https://test1.live.sdetv.com.cn/live/dianshizhibo/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Dragon TV 4K (东方卫视4K)",
+    nameLocalized: { zhcn: "东方卫视4K" },
+    urls: [
+      "https://bp-resource-dfl.bestv.cn/148/3/video.m3u8"
+    ]
+  },
+  {
+    name: "Suzhou TV 4K (苏州4K)",
+    nameLocalized: { zhcn: "苏州4K" },
+    urls: [
+      "https://tylive.kan0512.com/norecord/csztv4k_4k.m3u8"
+    ]
+  },
+  {
+    name: "Harbin Film & TV (哈尔滨影视)",
+    nameLocalized: { zhcn: "哈尔滨影视" },
+    urls: [
+      "https://stream.hrbtv.net/yspd/playlist.m3u8",
+      "https://stream.hrbtv.net/yspd/sd/live.m3u8"
+    ]
+  },
+  {
+    name: "Yanbian Satellite TV (延边卫视)",
+    nameLocalized: { zhcn: "延边卫视" },
+    urls: [
+      "https://srs.iyb983.cn/video/CYS/index.m3u8"
+    ]
+  },
+  {
+    name: "Anduo TV – Life Service (安多卫视生活服务)",
+    nameLocalized: { zhcn: "安多卫视生活服务" },
+    urls: [
+      "https://liveout.xntv.tv/a65jur/90p2i1.m3u8?zqinghd",
+      "https://liveout.xntv.tv/a65jur/90p2i1.m3u8?zqing"
+    ]
+  },
+  {
+    name: "Anduo TV – News (安多卫视新闻综合)",
+    nameLocalized: { zhcn: "安多卫视新闻综合" },
+    urls: [
+      "https://liveout.xntv.tv/a65jur/96iln2.m3u8"
+    ]
+  },
+  {
+    name: "Dongguan News (东莞新闻综合)",
+    nameLocalized: { zhcn: "东莞新闻综合" },
+    urls: [
+      "https://stream.sun0769.com/dgrtv1/mp4tv1_800/index.m3u8"
+    ]
+  },
+  {
+    name: "Dongguan Life (东莞生活资讯)",
+    nameLocalized: { zhcn: "东莞生活资讯" },
+    urls: [
+      "https://stream.sun0769.com/dgrtv1/mp4tv2_800/index.m3u8"
+    ]
+  },
+  {
+    name: "Zhejiang News (中国蓝新闻)",
+    nameLocalized: { zhcn: "中国蓝新闻" },
+    urls: [
+      "https://ali-m-l.cztv.com/channels/lantian/channel009/1080p.m3u8"
+    ]
+  },
+  {
+    name: "Zhejiang Documentary (之江纪录)",
+    nameLocalized: { zhcn: "之江纪录" },
+    urls: [
+      "https://ali-m-l.cztv.com/channels/lantian/channel012/1080p.m3u8",
+      "https://ali-m-l.cztv.com/channels/lantian/channel12/720p.m3u8"
+    ]
+  },
+  {
+    name: "Renshou Comprehensive (仁寿综合)",
+    nameLocalized: { zhcn: "仁寿综合" },
+    urls: [
+      "https://play.scrstv.com.cn/DT/live.m3u8?auth_key=60001724663204-0-0-c1cc4ded9841ac34f63cdbd3aec647ef"
+    ]
+  },
+  {
+    name: "Yuyao Culture 1 (余姚姚江文化1)",
+    nameLocalized: { zhcn: "余姚姚江文化1" },
+    urls: [
+      "https://l.cztvcloud.com/channels/lantian/SXyuyao2/720p.m3u8"
+    ]
+  },
+  {
+    name: "Yuyao Culture 2 (余姚姚江文化2)",
+    nameLocalized: { zhcn: "余姚姚江文化2" },
+    urls: [
+      "https://l.cztvcloud.com/channels/lantian/SXyuyao3/720p.m3u8"
+    ]
+  },
+  {
+    name: "Yuyao Comprehensive (余姚综合)",
+    nameLocalized: { zhcn: "余姚综合" },
+    urls: [
+      "https://l.cztvcloud.com/channels/lantian/SXyuyao1/720p.m3u8"
+    ]
+  },
+  {
+    name: "Jiange Comprehensive (剑阁综合)",
+    nameLocalized: { zhcn: "剑阁综合" },
+    urls: [
+      "https://m3u8.channel.dzsm.com/nmip-media/channellive/channel105549/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Beijing News (北京新闻)",
+    nameLocalized: { zhcn: "北京新闻" },
+    urls: [
+      "https://satellitepull.cnr.cn/live/wxbjxwgb/playlist.m3u8",
+      "https://brtv-radiolive.rbc.cn/alive/fm945.m3u8"
+    ]
+  },
+  {
+    name: "Beijing Life (北京生活)",
+    nameLocalized: { zhcn: "北京生活" },
+    urls: [
+      "https://wstvcpudali.v.myalicdn.com/wstvcpud/udrmbtv7_1/index.m3u8?adapt=0&BR=audio"
+    ]
+  },
+  {
+    name: "Guxian Comprehensive (古县综合)",
+    nameLocalized: { zhcn: "古县综合" },
+    urls: [
+      "https://l2fpdktp.live.sxmty.com/live/hls/e4c3b363cc4549788e2d983f403e07db/d99ce1eb686e41b9afc888110bd95aa7.m3u8?zshanxd"
+    ]
+  },
+  {
+    name: "Harbin Life (哈尔滨生活)",
+    nameLocalized: { zhcn: "哈尔滨生活" },
+    urls: [
+      "https://stream.hrbtv.net/shpd/playlist.m3u8",
+      "https://stream.hrbtv.net/shpd/sd/live.m3u8"
+    ]
+  },
+  {
+    name: "Taigu News (太谷新闻综合)",
+    nameLocalized: { zhcn: "太谷新闻综合" },
+    urls: [
+      "https://p2.vzan.com/slowlive/596867413819827251/live.m3u8?zbid=1725814272&tpid=1516989100&type=0"
+    ]
+  },
+  {
+    name: "Guangyuan Public (广元公共)",
+    nameLocalized: { zhcn: "广元公共" },
+    urls: [
+      "https://m3u8.channel.dzsm.com/nmip-media/channellive/channel101257/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Guangyuan Comprehensive (广元综合)",
+    nameLocalized: { zhcn: "广元综合" },
+    urls: [
+      "https://m3u8.channel.dzsm.com/nmip-media/channellive/channel100999/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Guangzhou Comprehensive (广州综合)",
+    nameLocalized: { zhcn: "广州综合" },
+    urls: [
+      "https://tencentplay.gztv.com/live/zonghes.m3u8?txSecret=f0147ae6453a80f5989f382368b72aad&txTime=18d00acf487"
+    ]
+  },
+  {
+    name: "Xinchang News (新昌新闻综合)",
+    nameLocalized: { zhcn: "新昌新闻综合" },
+    urls: [
+      "https://l.cztvcloud.com/channels/lantian/SXxinchang1/720p.m3u8"
+    ]
+  },
+  {
+    name: "Wuxi Entertainment (无锡娱乐)",
+    nameLocalized: { zhcn: "无锡娱乐" },
+    urls: [
+      "https://stream.thmz.com/wxtv2/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Wuxi Life (无锡生活)",
+    nameLocalized: { zhcn: "无锡生活" },
+    urls: [
+      "https://stream.thmz.com/wxtv4/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Wuxi Economy (无锡经济)",
+    nameLocalized: { zhcn: "无锡经济" },
+    urls: [
+      "https://stream.thmz.com/wxtv5/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Yimen Comprehensive (易门综合)",
+    nameLocalized: { zhcn: "易门综合" },
+    urls: [
+      "https://zb-live.ynurl.com/yimen/3b75a2f9-7941-46b7-992d-a9796fbca5fe.m3u8"
+    ]
+  },
+  {
+    name: "Chaotian Comprehensive (朝天综合)",
+    nameLocalized: { zhcn: "朝天综合" },
+    urls: [
+      "https://m3u8.channel.dzsm.com/nmip-media/channellive/channel104601/playlist.m3u8"
+    ]
+  },
+  {
+    name: "Wujin News (武进新闻)",
+    nameLocalized: { zhcn: "武进新闻" },
+    urls: [
+      "https://live.wjyanghu.com/live/CH1.m3u8"
+    ]
+  }
 ];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pwa-player-cache-v41";
+const CACHE_NAME = "pwa-player-cache-v42";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
