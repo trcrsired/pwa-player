3b90f0e38f151d58fa795db86cca17c9ce47808b
trcrsired <uwgghhbcad@gmail.com>
2026-03-18 23:44:09 +0800

    kill the idea of autoplay since it is impossible

--- player.js
+++ player.js
@@ -538,37 +538,13 @@ video.addEventListener("error", () => {
     }, 2000);
 });
 
-let launchedWithFile = false;
-
 if ('launchQueue' in window) {
   launchQueue.setConsumer(async (launchParams) => {
     for (const fileHandle of launchParams.files) {
       const file = await fileHandle.getFile();
-      launchedWithFile = true;
       play_source(file);
       return;
       // Route to appropriate player logic
     }
   });
 }
-/*
-async function maybeAutoplayOnStart() {
-    if (launchedWithFile) return; // skip autoplay
-
-    const autoplay = localStorage.getItem("autoplay_on_start") === "true";
-    if (autoplay) {
-      try
-      {
-        togglePlayBtn(); // your existing play/pause logic
-      }
-      catch
-      {
-      }
-    }
-}
-
-document.addEventListener("DOMContentLoaded", async () =>
-{
-  maybeAutoplayOnStart();
-});
-*/
